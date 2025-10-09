8a4ca5716d64a8b49ae973fc16152a4b902fabee
Alex <mralexblood@outlook.com>
2025-10-09 10:23:37 +0800

    completed swarm setup

--- src/app/p2p/service.rs
+++ src/app/p2p/service.rs
@@ -1,13 +1,16 @@
-use anyhow::Error;
+use core::error;
+use std::{hash::{DefaultHasher, Hash, Hasher}, path::{Path, PathBuf}, time::{Duration, SystemTime, UNIX_EPOCH}};
+
 use async_trait::async_trait;
-use libp2p::{
-    dcutr, gossipsub, identify, identity::Keypair, kad::{self, store::MemoryStore}, mdns, ping, relay, request_response::cbor, swarm::NetworkBehaviour, Swarm
-};
-use thiserror::Error;
+use libp2p::{dcutr, gossipsub, identify, identity::{DecodingError, Keypair}, kad::{self, store::MemoryStore}, mdns, noise, ping, relay, request_response::{self, cbor}, swarm::NetworkBehaviour, tcp, yamux, StreamProtocol, Swarm};
 use serde::{Deserialize, Serialize};
+use thiserror::Error;
+use tokio::io;
 use tokio_util::sync::CancellationToken;
 
-use crate::app::Service;
+use crate::app::{ServerError, Service};
+
+use super::{config::P2pServiceConfig};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct FileChunkRequest {
@@ -21,7 +24,16 @@ pub struct FileChunkResponse {
 
 #[derive(Debug, Error)]
 pub enum P2pNetworkError {
-   
+    #[error("Failed to get directory of the keypair file: {0}")]
+    FailedToGetKeypairFileDir(PathBuf),
+    #[error("I/O error: {0}")]
+    IO(#[from] io::Error),
+    #[error("Keypair decoding error: {0}")]
+    KeypairDecoding(#[from] DecodingError),
+    #[error("Libp2p noise error: {0}")]
+    Libp2pNoise(#[from] libp2p::noise::Error),
+    #[error("Libp2p swarm builder error: {0}")]
+    Libp2pSwarmBuilder(String),
 }
 
 #[derive(NetworkBehaviour)]
@@ -30,40 +42,120 @@ pub struct P2pNetworkBehaviour {
     identify: identify::Behaviour,
     mdns: mdns::Behaviour<mdns::tokio::Tokio>,
     kademlia: kad::Behaviour<MemoryStore>,
+    gossipsub: gossipsub::Behaviour,
     relay_server: relay::Behaviour,
     relay_client: relay::client::Behaviour,
-    gossipsub: gossipsub::Behaviour,
     dcutr: dcutr::Behaviour,
     file_download: cbor::Behaviour<FileChunkRequest, FileChunkResponse>,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct P2pServiceConfig {
-    
-}
-
+#[derive(Debug)]
 pub struct P2pService {
-
+    config: P2pServiceConfig,
 }
 
 impl P2pService {
-    pub fn new() -> Self {
-        Self {}
+    pub fn new(config: P2pServiceConfig) -> Self {
+        Self{
+            config
+        }
     }
 
-    async fn keypair(&self)
+    async fn keypair(&self) -> Result<Keypair, P2pNetworkError> {
+        match tokio::fs::read(&self.config.keypair_file).await {
+            Ok(data) => {
+                Ok(
+                    Keypair::from_protobuf_encoding(data.as_slice())?
+                )
+            },
+            Err(_) => {
+                let keypair = Keypair::generate_ed25519();
+                let encoded = keypair.to_protobuf_encoding()?;
+                let dir = self.config.keypair_file.parent()
+                .ok_or(P2pNetworkError::FailedToGetKeypairFileDir(self.config.keypair_file.to_path_buf()))?;
+                let _ = tokio::fs::remove_file(&self.config.keypair_file).await;
+                tokio::fs::create_dir_all(dir).await?;
+                tokio::fs::write(&self.config.keypair_file, encoded).await?;
+                Ok(keypair)
+            },
+        }
+    }
 
-    fn swarm() -> Result<Swarm<P2pNetworkBehaviour>, P2pNetworkError> {
-        
-        let keypair = Keypair::generate_ed25519();
+    async fn swarm(&self) -> Result<Swarm<P2pNetworkBehaviour>, P2pNetworkError> {
+        let keypair = self.keypair().await?;
         let mut swarm = libp2p::SwarmBuilder::with_existing_identity(keypair)
-    }
+        .with_tokio()
+        .with_tcp(
+            tcp::Config::default(),
+             noise::Config::new,
+              yamux::Config::default)?
+        .with_relay_client(noise::Config::new, yamux::Config::default)?
+        .with_behaviour(|key_pair, relay_client| {
+            // kademlia config
+            let mut kad_config = kad::Config::new(StreamProtocol::new("/dfs/1.0.0/kad"));
+            kad_config.set_periodic_bootstrap_interval(Some(Duration::from_secs(30)));
+
+            // gossipsub config
+            let gossipsub_config = gossipsub::ConfigBuilder::default()
+            .heartbeat_interval(Duration::from_secs(10))
+            .validation_mode(gossipsub::ValidationMode::Strict)
+            .message_id_fn(|message| {
+                let mut hasher = DefaultHasher::new();
+                message.data.hash(&mut hasher);
+                message.topic.hash(&mut hasher);
+                if let Some(peer_id) = message.source {
+                    peer_id.hash(&mut hasher);
+                }
+                let now = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis();
+                now.to_string().hash(&mut hasher);
+                gossipsub::MessageId::from(hasher.finish().to_string())
+            })
+            .build()?;
+
+            Ok(
+                P2pNetworkBehaviour {
+                    ping: ping::Behaviour::new(ping::Config::default()),
+                    identify: identify::Behaviour::new(
+                        identify::Config::new("/dfs/1.0.0".to_string(), key_pair.public()),
+                    ),
+                    mdns: mdns::Behaviour::<mdns::tokio::Tokio>::new(
+                        mdns::Config::default(), 
+                        key_pair.public().to_peer_id(),
+                    )?,
+                    kademlia: kad::Behaviour::with_config(
+                        key_pair.public().to_peer_id(), 
+                        MemoryStore::new(key_pair.public().to_peer_id()),
+                        kad_config,
+                    ),
+                    gossipsub: gossipsub::Behaviour::new(
+                        gossipsub::MessageAuthenticity::Signed(key_pair.clone()),
+                        gossipsub_config,
+                    )?,
+                    relay_server: relay::Behaviour::new(key_pair.public().to_peer_id(), relay::Config::default()),
+                    relay_client,
+                    dcutr: dcutr::Behaviour::new(key_pair.public().to_peer_id()),
+                    file_download: cbor::Behaviour::new([(
+                        StreamProtocol::new("/dfs/1.0.0/file-download"),
+                        request_response::ProtocolSupport::Full,
+                    )], request_response::Config::default(),
+                ),
+                }
+            )
+        }).map_err(|error| P2pNetworkError::Libp2pSwarmBuilder(error.to_string()))?
+        .with_swarm_config(|config| config.with_idle_connection_timeout(Duration::from_secs(30)))
+        .build();
+    
+    Ok(swarm)
+}
 }
 
 #[async_trait]
 impl Service for P2pService {
-    async fn start(&self, cancel_token: CancellationToken) -> Result<(), Error> {
-        // TODO: Implement
+    async fn start(&self, cancel_token: CancellationToken) -> Result<(), ServerError> {
+        let swarm = self.swarm().await?;
+
+        // TODO: implement rest
+
         Ok(())
     }
 }
--- src/app/server.rs
+++ src/app/server.rs
@@ -2,26 +2,24 @@ use std::sync::Arc;
 
 use async_trait::async_trait;
 use thiserror::Error;
-use tokio::{
-    select,
-    sync::Mutex,
-    task::{JoinError, JoinSet},
-};
+use tokio::{select, sync::Mutex, task::{JoinError, JoinHandle, JoinSet}};
 use tokio_util::sync::CancellationToken;
 
-use crate::app::P2pService;
+use super::{config::P2pServiceConfig, service::{P2pNetworkError, P2pService}};
 
 #[derive(Debug, Error)]
 pub enum ServerError {
     #[error("Task join error: {0}")]
     TaskJoin(#[from] JoinError),
+    #[error("P2P network error: {0}")]
+    P2pNetwork(#[from] P2pNetworkError),
 }
 
 pub type ServerResult<T> = Result<T, ServerError>;
 
 pub struct Server {
     cancel_token: CancellationToken,
-    subtasks: Arc<Mutex<JoinSet<Result<(), ServerError>>>>,
+    subtasks: Arc<Mutex<Vec<JoinHandle<Result<(), ServerError>>>>>,
 }
 
 #[async_trait]
@@ -31,37 +29,40 @@ pub trait Service: Send + Sync + 'static {
 
 impl Server {
     pub fn new() -> Self {
-        Self {
-            cancel_token: CancellationToken::new(),
-            subtasks: Arc::new(Mutex::new(JoinSet::new())),
-        }
+        Self {cancel_token: CancellationToken::new(), subtasks: Arc::new(Mutex::new(vec![])) }
     }
 
     pub async fn start(&self) -> ServerResult<()> {
-        // p2p
-        let p2p_Service = P2pService::new();
-        self.spawn_task(p2p_Service).await?;
+        // p2p service
+        let p2p_service = P2pService::new(
+            P2pServiceConfig::builder()
+            .with_keypair_file("./keys.keypair")
+            .build()
+        );
+        self.spawn_task(p2p_service).await?;
+
         Ok(())
     }
 
     async fn spawn_task<S: Service>(&self, service: S) -> ServerResult<()> {
-        // lockj over subtasks
-        let mut join_set = self.subtasks.lock().await;
-
-        // spawn tasks
+        let mut handles = self.subtasks.lock().await;
         let cancel_token = self.cancel_token.clone();
-        join_set.spawn(async move { service.start(cancel_token).await });
+        handles.push(
+            tokio::spawn(async move {
+                service.start(cancel_token).await
+            })
+    );
 
         Ok(())
     }
 
-    /// Stops the server
+    /// Stops the server.
     pub async fn stop(&self) -> ServerResult<()> {
         println!("Shutting down...");
         self.cancel_token.cancel();
-        let mut tasks = self.subtasks.lock().await;
-        while let Some(res) = tasks.join_next().await {
-            res??;
+        let mut handles = self.subtasks.lock().await;
+        for handle in handles.iter_mut() {
+            handle.await??;
         }
         Ok(())
     }
